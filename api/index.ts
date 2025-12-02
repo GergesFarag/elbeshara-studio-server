81be6937ddcf8edaedd995b2766779a3e4454124
GergesFarag <georgefarag128@gmail.com>
2025-12-02 21:46:32 +0200

    fix:api index

--- api/index.ts
+++ api/index.ts
@@ -1,19 +1,30 @@
-import { Server } from 'http';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../src/app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
+import { ValidationPipe } from '@nestjs/common';
+import { ResponseInterceptor } from '../src/common/interceptors/response.interceptor';
+import express, { Express } from 'express';
 
-let cachedServer: Server;
+let cachedApp: Express;
 
 export default async function handler(req, res) {
-  if (!cachedServer) {
+  if (!cachedApp) {
     const expressApp = express();
     const adapter = new ExpressAdapter(expressApp);
     const app = await NestFactory.create(AppModule, adapter);
+    app.setGlobalPrefix('api/v1');
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
+    app.useGlobalInterceptors(new ResponseInterceptor());
+    app.enableCors();
     await app.init();
-    cachedServer = expressApp.listen(0);
+    cachedApp = expressApp;
   }
 
-  return cachedServer.emit('request', req, res);
+  return cachedApp(req, res);
 }
