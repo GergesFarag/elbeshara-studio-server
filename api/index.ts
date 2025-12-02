506df3a7061b14d5e32ede3092a91d59c410caad
GergesFarag <georgefarag128@gmail.com>
2025-12-02 21:48:49 +0200

    test:removing prefix

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../src/app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import { ValidationPipe } from '@nestjs/common';
-import { ResponseInterceptor } from '../src/common/interceptors/response.interceptor';
 import express, { Express } from 'express';
 
 let cachedApp: Express;
@@ -12,16 +10,6 @@ export default async function handler(req, res) {
     const expressApp = express();
     const adapter = new ExpressAdapter(expressApp);
     const app = await NestFactory.create(AppModule, adapter);
-    app.setGlobalPrefix('api/v1');
-    app.useGlobalPipes(
-      new ValidationPipe({
-        whitelist: true,
-        forbidNonWhitelisted: true,
-        transform: true,
-      }),
-    );
-    app.useGlobalInterceptors(new ResponseInterceptor());
-    app.enableCors();
     await app.init();
     cachedApp = expressApp;
   }
